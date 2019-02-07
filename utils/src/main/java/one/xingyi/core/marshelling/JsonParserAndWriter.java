c5d5d23e5f9f0fa240d9f02b07518d1aa9baebe1
Phil <phil.rice@iee.org>
2019-02-07 11:13:58 +0100

    wip

--- utils/src/main/java/one/xingyi/core/marshelling/JsonParserAndWriter.java
+++ utils/src/main/java/one/xingyi/core/marshelling/JsonParserAndWriter.java
@@ -1,4 +1,5 @@
 package one.xingyi.core.marshelling;
+import one.xingyi.core.client.IResourceList;
 import one.xingyi.core.optics.Lens;
 public interface JsonParserAndWriter<J> extends JsonParser<J>, JsonWriter<J> {
     Lens<J, J> lensToChild(String childname);
@@ -6,4 +7,5 @@ public interface JsonParserAndWriter<J> extends JsonParser<J>, JsonWriter<J> {
     Lens<J, Double> lensToDouble(String name);
 
     Lens<J, Integer> lensToInteger(String name);
+    <T>Lens<J, IResourceList<T>> lensToResourceList(String name);
 }
--- utils/src/main/java/one/xingyi/core/optics/lensLanguage/ListLensDefn.java
+++ utils/src/main/java/one/xingyi/core/optics/lensLanguage/ListLensDefn.java
@@ -3,15 +3,21 @@ package one.xingyi.core.optics.lensLanguage;
 import lombok.EqualsAndHashCode;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+import one.xingyi.core.client.IResourceList;
+import one.xingyi.core.marshelling.JsonParserAndWriter;
+import one.xingyi.core.optics.Lens;
+
+import java.util.List;
 
 @ToString
 @EqualsAndHashCode
 @RequiredArgsConstructor
-public class ListLensDefn implements LensDefn {
+public class ListLensDefn <T>implements LensDefn<IResourceList<T>> {
     public final String name;
     public final String childClassName;
     @Override public String name() { return name; }
     @Override public String asString() {
         return name + "/*" + childClassName;
     }
+    @Override public <J> Lens<J, IResourceList<T>> asLens(JsonParserAndWriter<J> json) { return json.<T>lensToResourceList(name); }
 }
