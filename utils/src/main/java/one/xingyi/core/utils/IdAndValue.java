7de0caafc9f9da2c9835a8d7d364357ebdef0a48
Phil <phil.rice@iee.org>
2019-02-10 10:16:08 +0100

    Added getters to idandvalue

--- utils/src/main/java/one/xingyi/core/utils/IdAndValue.java
+++ utils/src/main/java/one/xingyi/core/utils/IdAndValue.java
@@ -1,14 +1,18 @@
 package one.xingyi.core.utils;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import one.xingyi.core.marshelling.ContextForJson;
 import one.xingyi.core.marshelling.HasJson;
 import one.xingyi.core.marshelling.JsonWriter;
 @Getter
 @RequiredArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class IdAndValue<T> {
-  public  final String id;
-    public   final T t;
+    public final String id;
+    public final T t;
     static public <J, T extends HasJson<ContextForJson>> J toJson(IdAndValue<T> t, JsonWriter<J> jsonTc, ContextForJson contextForJson) {
         return jsonTc.makeObject("id", t.id, "value", t.t.toJson(jsonTc, contextForJson));
     }
