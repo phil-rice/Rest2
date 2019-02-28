8f077747f4fbc8eeb094bacfdee7e843dace693b
Phil <phil.rice@iee.org>
2019-02-28 11:56:42 +0100

    Wip tidying up annotation processor

--- utils/src/main/java/one/xingyi/core/annotationProcessors/XingYiAnnotationProcessor.java
+++ utils/src/main/java/one/xingyi/core/annotationProcessors/XingYiAnnotationProcessor.java
@@ -7,7 +7,8 @@ import one.xingyi.core.filemaker.*;
 import one.xingyi.core.monad.CompletableFutureDefn;
 import one.xingyi.core.monad.MonadDefn;
 import one.xingyi.core.names.*;
-import one.xingyi.core.sdk.IXingYiResource;
+import one.xingyi.core.sdk.IXingYiResourceDefn;
+import one.xingyi.core.sdk.IXingYiViewDefn;
 import one.xingyi.core.utils.*;
 import one.xingyi.core.validation.Result;
 
@@ -18,7 +19,6 @@ import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
-import javax.tools.Diagnostic;
 import javax.tools.FileObject;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardLocation;
@@ -26,6 +26,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.annotation.Annotation;
 import java.util.*;
 import java.util.function.Function;
 
@@ -64,15 +65,29 @@ public class XingYiAnnotationProcessor extends AbstractProcessor {
     Valid<ElementFail, TypeElement> checkNameStartsWithI = check(e -> e.getSimpleName().toString().startsWith("I"), e -> "Name must start with an I");
     Valid<ElementFail, TypeElement> checkNameEndsWithDefn = check(e -> e.getSimpleName().toString().endsWith("Defn"), e -> "Name must end with Defn");
     Valid<ElementFail, TypeElement> checkImplements(String className) {return check(e -> Lists.find(e.getInterfaces(), i -> i.toString().startsWith(className)).isPresent(), e -> "Must extend " + className);}
-    Valid<ElementFail, Element> initialTypeElementChecks = Valid.compose(e -> (TypeElement) e,
-            checkElementIsAnInterface, checkNameStartsWithI, checkNameEndsWithDefn, checkImplements(IXingYiResource.class.getName()));
+    Valid<ElementFail, Element> initialTypeElementChecks(Class<?> obligatoryInterface) {
+        return Valid.compose(e -> (TypeElement) e,
+                checkElementIsAnInterface, checkNameStartsWithI, checkNameEndsWithDefn, checkImplements(obligatoryInterface.getName()));
+    }
 
-
-    private List<Result<ElementFail, ResourceDom>> makeResourceDomResults(RoundEnvironment env, LoggerAdapter log, ElementToBundle bundle) {
-        List<Element> elements = Sets.sortedList((Set<Element>) env.getElementsAnnotatedWith(Resource.class), comparator());
-        List<ElementFail> elementFails = Valid.checkAll(elements, initialTypeElementChecks);
+    private List<Element> getCheckedElements(Class<? extends Annotation> annotation, Class<?> obligatoryInterface, RoundEnvironment env, LoggerAdapter log) {
+        List<Element> elements = Sets.sortedList((Set<Element>) env.getElementsAnnotatedWith(annotation), comparator());
+        List<ElementFail> elementFails = Valid.checkAll(elements, initialTypeElementChecks(obligatoryInterface));
         Lists.foreach(elementFails, log::error);
-        return Lists.map(elements, e -> bundle.elementToEntityDom(bundle.elementToEntityNames().apply(e)).apply((TypeElement) e));
+        return elements;
+    }
+
+    private List<ResourceDom> makeResourceDomResults(RoundEnvironment env, LoggerAdapter log, ElementToBundle bundle) {
+        List<Element> elements = getCheckedElements(Resource.class, IXingYiResourceDefn.class, env, log);
+        return log.logFailuresAndReturnSuccesses(Lists.map(elements, e -> bundle.elementToEntityDom(bundle.elementToEntityNames().apply(e)).apply((TypeElement) e)));
+    }
+    private List<ViewDom> makeViewDoms(RoundEnvironment env, LoggerAdapter log, ElementToBundle bundle) {
+        List<Element> viewElements = getCheckedElements(View.class, IXingYiViewDefn.class, env, log);
+        return log.logFailuresAndReturnSuccesses(Lists.map(viewElements,
+                v -> bundle.elementToViewNames().apply((TypeElement) v).flatMap(vn -> bundle.elementToViewDom(vn).apply((TypeElement) v))));
+    }
+    Result<String, FileDefn> makeServer(ServerDom serverDom) {
+        return new ServerFileMaker().apply(serverDom);
     }
 
 
@@ -84,54 +99,19 @@ public class XingYiAnnotationProcessor extends AbstractProcessor {
         ElementToBundle bundle = ElementToBundle.simple(log);
         log.info("Processing XingYi Annotations");
         try {
-            List<Result<ElementFail, ResourceDom>> resourceDomsResults = makeResourceDomResults(env, log, bundle);
-            List<ResourceDom> resourceDoms = Result.successes(resourceDomsResults);
-            log.info("Made entityDoms: " + resourceDoms);
-
-
-            List<? extends Element> viewElements = Sets.sortedList(env.getElementsAnnotatedWith(View.class), comparator());
-            log.info("Making viewDoms elements: " + viewElements);
-            List<Result<ElementFail, ViewDom>> viewDomResults = Lists.map(viewElements,
-                    v -> bundle.elementToViewNames().apply((TypeElement) v).flatMap(vn ->
-                            bundle.elementToViewDom(vn).apply((TypeElement) v)
-                    )
-            );
-            val viewDoms = Result.successes(viewDomResults);
-            log.info("Made viewDoms: " + Lists.map(viewDoms, vd-> vd.viewNames.clientView.asString()));
-
-            //TODO Work out how to spot at this stage or before if there are classes in the names of fields in views. Best done when the element is available
-
-            CodeDom codeDom = new CodeDom(monadDefn, resourceDoms, viewDoms);
+            CodeDom codeDom = new CodeDom(monadDefn, makeResourceDomResults(env, log, bundle), makeViewDoms(env, log, bundle));
             log.info("Made codeDom: " + codeDom);
 
-            ResultAndFailures<String, List<FileDefn>> codeContentAndIssues = makeContent(log,codeDom);
-            for (String issue : codeContentAndIssues.failures) {
-                log.error(issue);
-            }
-            List<FileDefn> codeContent = codeContentAndIssues.t;
-            val serverElements = Sets.toList(env.getElementsAnnotatedWith(Server.class));
-//            val getElements = Sets.toList((Set<TypeElement>) env.getElementsAnnotatedWith(Get.class));
+            List<FileDefn> codeContent = makeContent(log, codeDom);
+            List<ServerDom> serverDoms = log.logFailuresAndReturnSuccesses(Lists.map(Sets.toList(env.getElementsAnnotatedWith(Server.class)), e1 -> ServerDom.create(names, e1, codeDom)));
+            List<FileDefn> systemContent = log.logFailuresAndReturnStringSuccesses(Lists.map(serverDoms, sd -> makeServer(sd)));
 
-            List<Result<ElementFail, ServerDom>> serverDomResults = Lists.map(serverElements, e -> ServerDom.create(names, e, codeDom));
-            List<ServerDom> serverDoms = Result.successes(serverDomResults);
-            List<Result<String, FileDefn>> systemContentResult = Lists.map(serverDoms, sd -> makeServer(sd));
-            for (String issue : Result.failures(systemContentResult))
-                log.error(issue);
-            Result<String, FileDefn> t = makeHttpService("one.xingyi.core.httpClient", monadDefn);
-            t.forEach(x -> {try { makeClassFile(x);} catch (Exception e) {}});
-            List<FileDefn> systemContent = Result.successes(systemContentResult);
-            log.info("Found the following system contents" + Lists.mapJoin(systemContent, ",", s -> s.packageAndClassName.asString()));
-
-            for (FileDefn fileDefn : Lists.append(systemContent, codeContent)) {
-//                log.info("making scode or system content "+fileDefn.packageAndClassName.className);
-                makeClassFile(fileDefn);
-//                log.info("   ... finished "+fileDefn.packageAndClassName.className);
-            }
+            makeHttpService("one.xingyi.core.httpClient", monadDefn).forEach(x -> {
+                try { makeClassFile(x);} catch (Exception e) {}  //TODO WHy do we have this
+            });
+            Lists.foreach(Lists.append(systemContent, codeContent), this::makeClassFile);
             log.info("got to end of creation");
-            for (ElementFail fail : Lists.append(Result.failures(resourceDomsResults), Result.failures(viewDomResults), Result.failures(serverDomResults)))
-                Optionals.doit(fail.optElement, () -> log.error(fail.message + "no element"), e -> log.error(e, fail.message + " element " + e));
             validateLens(env, log, codeDom);
-
         } catch (
                 Exception e) {
             Throwable unwrapped = WrappedException.unWrap(e);
@@ -139,15 +119,12 @@ public class XingYiAnnotationProcessor extends AbstractProcessor {
         }
         return false;
     }
-    Result<String, FileDefn> makeServer(ServerDom serverDom) {
-        return new ServerFileMaker().apply(serverDom);
-    }
 
     Result<String, FileDefn> makeHttpService(String packageName, MonadDefn monadDefn) {
         return new HttpServiceFileMaker(packageName).apply(monadDefn);
     }
 
-    ResultAndFailures<String, List<FileDefn>> makeContent(LoggerAdapter log, CodeDom codeDom) {
+    List<FileDefn> makeContent(LoggerAdapter log, CodeDom codeDom) {
         List<IFileMaker<ResourceDom>> entityFileMakes = Arrays.asList(
                 new CodeDomDebugFileMaker(),
                 new ServerInterfaceFileMaker(),
@@ -175,13 +152,15 @@ public class XingYiAnnotationProcessor extends AbstractProcessor {
         List<FileDefn> fromViewDom = Result.successes(fromViewDomResults);
 
 
-        return new ResultAndFailures<>(Lists.append(fromCodeDomIssues, fromViewDomIssues), Lists.<FileDefn>append(fromCodeDom, fromViewDom));
+        List<String> issues = Lists.append(fromCodeDomIssues, fromViewDomIssues);
+        Lists.foreach(issues, log::error);
+        return Lists.<FileDefn>append(fromCodeDom, fromViewDom);
     }
 
     void makeClassFile(FileDefn fileDefn) {
         WrappedException.wrap(() -> {
             JavaFileObject builderFile = filer.createSourceFile(fileDefn.packageAndClassName.asString());
-            messager.printMessage(Diagnostic.Kind.NOTE, "making  " + fileDefn.packageAndClassName + "->" + builderFile.toUri());
+//            messager.printMessage(Diagnostic.Kind.NOTE, "making  " + fileDefn.packageAndClassName + "->" + builderFile.toUri());
             Files.setText(() -> new PrintWriter(builderFile.openWriter()), fileDefn.content);
         });
     }
--- utils/src/main/java/one/xingyi/core/utils/LoggerAdapter.java
+++ utils/src/main/java/one/xingyi/core/utils/LoggerAdapter.java
@@ -1,15 +1,26 @@
 package one.xingyi.core.utils;
 import one.xingyi.core.annotationProcessors.ElementFail;
+import one.xingyi.core.validation.Result;
 
 import javax.annotation.processing.Messager;
 import javax.lang.model.element.Element;
 import javax.tools.Diagnostic;
+import java.util.List;
 public interface LoggerAdapter {
     void info(String message);
     void info(Element element, String message);
     void warning(Element element, String message);
     void error(Element element, String message);
     default void error(ElementFail fail){ Optionals.doit(fail.optElement, ()-> error(fail.message), e-> error(e, fail.message)); }
+
+    default <T> List<T> logFailuresAndReturnSuccesses(List<Result<ElementFail, T>> list) {
+         Lists.foreach(Result.failures(list), this::error);
+         return Result.successes(list);
+    }
+    default <T> List<T> logFailuresAndReturnStringSuccesses(List<Result<String, T>> list) {
+         Lists.foreach(Result.failures(list), this::error);
+         return Result.successes(list);
+    }
     void error(String message);
     LoggerAdapter withElement(Element element);
     static LoggerAdapter fromMessager(Messager messager, Element element) {
